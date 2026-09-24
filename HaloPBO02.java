d07e19511973f1de09e78eade0b75f31b7608efe
Ali-syr <liakbarmatrix1@gmail.com>
2026-09-24 08:15:32 +0800

    Pengumpulan tugas pbo2 dengan program halo java, versi java, vendor dan os di gunakan

---
+++ HaloPBO02.java
@@ -0,0 +1,20 @@
+public class HaloPBO02  {
+    public static  void main (String[]args) {
+        System.out.println("=============================");
+        System.out.println("==  KARTU MAHASISWA PBO 2 ===");
+        System.out.println("=============================");
+
+        System.out.println("Nama      : Muhammad Ali Akbar                                 ");
+        System.out.println("NPM       : 2410010012                                         ");
+        System.out.println("Prodi     : Teknik Informatika                                 ");
+        System.out.println("Semester  : 5                                                  ");
+        System.out.println("Alasan    : Ingin mencoba mempelajari java lebih dalam         ");
+
+        System.out.println("Versi Java : " + System.getProperty("java.version"));
+        System.out.println("Vendor JDK : " + System.getProperty("java.vendor"));
+        System.out.println("Sistem operasi : " + System.getProperty("os.name"));
+
+
+    }
+    
+}
